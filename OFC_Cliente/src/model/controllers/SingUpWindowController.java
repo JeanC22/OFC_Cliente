87f493b5dee92a154be6c2d287e53ee22b35b731
iker <iker@192.168.1.135>
2022-10-25 20:12:59 +0200

    Validaciones de signUp

--- OFC_Cliente/src/model/controllers/SingUpWindowController.java
+++ OFC_Cliente/src/model/controllers/SingUpWindowController.java
@@ -7,12 +7,19 @@ package model.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -49,7 +56,48 @@ public class SingUpWindowController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        userNameTxTF.setTooltip(new Tooltip("Max characters 15"));
+        passwdTxPF.setTooltip(new Tooltip("min 6 and max 12 characters"));
+        fullNameTxTF.setTooltip(new Tooltip("Max 40 characters"));
+        eMailTxTF.setTooltip(new Tooltip("example@example.com"));
+        
+        
+        
+        
     }    
+
+    @FXML
+    private void signUp(ActionEvent event) {
+        String regex = "^[a-zA-Z1-9]*$";
+        
+        
+        if(this.userNameTxTF.getText().trim().equalsIgnoreCase("") ||
+                this.passwdTxPF.getText().trim().equalsIgnoreCase("") ||
+                this.eMailTxTF.getText().trim().equalsIgnoreCase("") ||
+                this.fullNameTxTF.getText().trim().equalsIgnoreCase("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR,"Uno de los campos"
+                    + " no esta informado.",ButtonType.OK);
+            alert.showAndWait();
+        }else if(!this.userNameTxTF.getText().matches(regex) || 
+                this.userNameTxTF.getText().toString().length() > 15){
+            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo UserName tiene "
+                    + "caracteres especiales o te has pasado de el limite de "
+                    + "caracteres permitidos(max 15)",ButtonType.OK);
+             alert.showAndWait();
+        }else if(this.passwdTxPF.getText().toString().length() < 6 ||
+                this.passwdTxPF.getText().toString().length() > 12 ){
+            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo Password "
+                    + "tiene que ser de minimo 6 caracteres y maximo de 12 caracteres"
+                    ,ButtonType.OK);
+            alert.showAndWait();
+        }else if(!this.fullNameTxTF.getText().matches(regex) || this.fullNameTxTF.getText().toString().length() > 40){
+            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo Fullname tiene "
+                    + "caracteres especiales o te has pasado de el limite de "
+                    + "caracteres permitidos(max 40)",ButtonType.OK);
+            alert.showAndWait();
+        }
+        
+    }
     
 }
