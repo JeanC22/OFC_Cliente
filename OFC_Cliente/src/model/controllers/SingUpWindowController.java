bda6e38227f6a1c2651798b6f3f5b82bcf3c27f0
iker <iker@192.168.1.135>
2022-10-27 13:34:53 +0200

    Validaciones de signUp acabadas y envio de usuario empezado

--- OFC_Cliente/src/model/controllers/SingUpWindowController.java
+++ OFC_Cliente/src/model/controllers/SingUpWindowController.java
@@ -28,6 +28,10 @@ import javafx.scene.input.MouseEvent;
  */
 public class SingUpWindowController implements Initializable {
 
+    private static String regex = "^[a-zA-Z1-9]*$";
+    private static String regexEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
     @FXML
     private TextField userNameTxTF;
     @FXML
@@ -56,48 +60,76 @@ public class SingUpWindowController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+
         userNameTxTF.setTooltip(new Tooltip("Max characters 15"));
         passwdTxPF.setTooltip(new Tooltip("min 6 and max 12 characters"));
         fullNameTxTF.setTooltip(new Tooltip("Max 40 characters"));
         eMailTxTF.setTooltip(new Tooltip("example@example.com"));
-        
-        
-        
-        
-    }    
+
+        Tooltip.install(userNameTT, new Tooltip("Max characters 15"));
+        Tooltip.install(passwdTT, new Tooltip("min 6 and max 12 characters"));
+        Tooltip.install(fullNameTT, new Tooltip("Max 40 characters"));
+        Tooltip.install(eMailTT, new Tooltip("example@example.com"));
+
+    }
 
     @FXML
     private void signUp(ActionEvent event) {
-        String regex = "^[a-zA-Z1-9]*$";
+        
+        User usu = new User();
+        IntefaceFactory fac = new InterfaceFactory();
+        DaoInteface dao = fac.getInterface();
         
         
-        if(this.userNameTxTF.getText().trim().equalsIgnoreCase("") ||
-                this.passwdTxPF.getText().trim().equalsIgnoreCase("") ||
-                this.eMailTxTF.getText().trim().equalsIgnoreCase("") ||
-                this.fullNameTxTF.getText().trim().equalsIgnoreCase("")){
-            Alert alert = new Alert(Alert.AlertType.ERROR,"Uno de los campos"
-                    + " no esta informado.",ButtonType.OK);
-            alert.showAndWait();
-        }else if(!this.userNameTxTF.getText().matches(regex) || 
-                this.userNameTxTF.getText().toString().length() > 15){
-            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo UserName tiene "
-                    + "caracteres especiales o te has pasado de el limite de "
-                    + "caracteres permitidos(max 15)",ButtonType.OK);
-             alert.showAndWait();
-        }else if(this.passwdTxPF.getText().toString().length() < 6 ||
-                this.passwdTxPF.getText().toString().length() > 12 ){
-            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo Password "
-                    + "tiene que ser de minimo 6 caracteres y maximo de 12 caracteres"
-                    ,ButtonType.OK);
-            alert.showAndWait();
-        }else if(!this.fullNameTxTF.getText().matches(regex) || this.fullNameTxTF.getText().toString().length() > 40){
-            Alert alert = new Alert(Alert.AlertType.ERROR,"El campo Fullname tiene "
-                    + "caracteres especiales o te has pasado de el limite de "
-                    + "caracteres permitidos(max 40)",ButtonType.OK);
-            alert.showAndWait();
-        }
+        
+        validations();
+        
+        usu.setLogin(this.userNameTxTF.getText());
+        usu.setPassword(this.passwdTxPF.getText());
+        usu.setfullName(this.fullNameTxTF.getText());
+        usu.setEmail(this.eMailTxTF.getText());
+        
+        dao.signUp(usu);
+        
         
     }
-    
+
+    private void validations() {
+        if (this.userNameTxTF.getText().trim().equalsIgnoreCase("")
+                || this.passwdTxPF.getText().trim().equalsIgnoreCase("")
+                || this.eMailTxTF.getText().trim().equalsIgnoreCase("")
+                || this.fullNameTxTF.getText().trim().equalsIgnoreCase("")) {
+
+            errorMessage("Uno de los campos no esta informado");
+
+        } else if (!this.userNameTxTF.getText().matches(regex)
+                || this.userNameTxTF.getText().toString().length() > 15) {
+
+            errorMessage("El campo UserName tiene caracteres "
+                    + "especiales o te has pasado de el limite de "
+                    + "caracteres permitidos(max 15)");
+
+        } else if (this.passwdTxPF.getText().toString().length() < 6
+                || this.passwdTxPF.getText().toString().length() > 12) {
+            errorMessage("El campo Password tiene que ser de minimo 6 "
+                    + "caracteres y maximo de 12 caracteres");
+        } else if (!this.fullNameTxTF.getText().matches(regex)
+                || this.fullNameTxTF.getText().toString().length() > 40) {
+
+            errorMessage("El campo Fullname tiene "
+                    + "caracteres especiales o te has pasado de el limite de "
+                    + "caracteres permitidos(max 40)");
+
+        } else if (!this.eMailTxTF.getText().matches(regexEmail)) {
+
+            errorMessage("El campo email notiene el formato adecuado"
+                    + "(example@example.com)");
+        }
+    }
+
+    private void errorMessage(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, mensaje, ButtonType.OK);
+        alert.showAndWait();
+    }
+
 }
