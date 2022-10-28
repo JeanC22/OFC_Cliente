2bec7d59655a4576a6a54b336039af6972125a96
2dam <2dam@HZ375745.TartangaLH.eus>
2022-10-28 11:56:52 +0200

    hay que realizar cambios en el controlador

--- OFC_Cliente/src/model/controllers/SingInWindowController.java
+++ OFC_Cliente/src/model/controllers/SingInWindowController.java
@@ -50,11 +50,14 @@ public class SingInWindowController{
     @FXML
     private Hyperlink signUpLink;
     
+    private static final Logger LOGGER = Logger.getLogger("model.controllers.SingInWindowController");
+    
     public void setStage(Stage stage) {
         this.stage = stage;
     }
     
     public void initStage(Parent root) {
+        LOGGER.info("Incializando la ventana de Sign In");
         //Create a scene associated to the node graph root.
         Scene scene = new Scene(root);
        
@@ -64,8 +67,7 @@ public class SingInWindowController{
         stage.setTitle("OFC SING IN");
         stage.setResizable(false);
         stage.setOnShowing(this::windowShowing);
-        userNameTxTF.textProperty().addListener((observable) -> this.userNameLength((ObservableValue) observable));
-        passwdTxTF.textProperty().addListener((observable) -> this.passwordLength((ObservableValue) observable));
+        userNameTxTF.textProperty().addListener(this::userNameLength);
         //Show window
         this.stage.show();
     }
@@ -102,69 +104,78 @@ public class SingInWindowController{
             Logger.getLogger(SingInWindowController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    /**
-     * User name validation length. If field is too long than 15 characters, show error message.
-     * Also checks that it does not contain special characters
-     * @param observable The value being observed.
-     */
-    private void userNameLength(ObservableValue observable) {
-        String regex = "^[a-zA-Z1-9]*$";
-        if (userNameTxTF.getText().length() > 15) {
-            new Alert(AlertType.ERROR, "La longitud del campo usuario supera los 15 caracteres").showAndWait();
-        }else if(!userNameTxTF.getText().matches(regex)){
-             new Alert(AlertType.ERROR, "El campo contiene caracteres especiales").showAndWait();
-        }
-
-    }
-    /**
-     * Password validation length. If field is less than 6 or long than 12 characters
-     * @param observable The value being observed.
-     */
-    private void passwordLength(ObservableValue observable) {
-        if (passwdTxTF.getText().length() == 6) {
-            new Alert(AlertType.ERROR, "La longitud minima del campo contraseña son 6 caracteres").showAndWait();
-        }else if(passwdTxTF.getText().length() > 12){
-            new Alert(AlertType.ERROR, "La longitud del campo contraseña supera el maximo de 12 caracteres").showAndWait();
-        }
-
-    }
-    /**
+    
+     /**
      * Action event for button SignIn. If fields user name and password is filled, show error message.
      * Otherwise, open the window Loged.
      * @param event 
      */
     @FXML
     private void signIn(ActionEvent event) {
+        passwdTxTF.textProperty().addListener(this::passwordLength);
         if (userNameTxTF.getText().isEmpty() || passwdTxTF.getText().isEmpty()) {
             new Alert(AlertType.ERROR, "Todos los campos no estan informados").showAndWait();
+        }else {
+            try {
+                Stage loginStage = new Stage();
 
-        } else {
+                URL viewLink = getClass().getResource("/model/views/LogedWindow.fxml");
+
+                FXMLLoader loader = new FXMLLoader(viewLink);
+                Parent root = (Parent) loader.load();
+                LogedWindowController logedStageController
+                        = ((LogedWindowController) loader.getController());
+                logedStageController.setStage(loginStage);
+                logedStageController.initStage(root);
                 try {
-                    Stage loginStage = new Stage();
-                    
-                    URL viewLink = getClass().getResource("/model/views/LogedWindow.fxml");
-                    
-                    FXMLLoader loader = new FXMLLoader(viewLink);
-                    Parent root = (Parent) loader.load();
-                    LogedWindowController logedStageController
-                            = ((LogedWindowController) loader.getController());
-                    logedStageController.setStage(loginStage);
-                    logedStageController.initStage(root);
-                    try {
-                        //close the actually View
-                        this.stage.close();
-                    } catch (Exception e) {
+                    //close the actually View
+                    this.stage.close();
+                } catch (Exception e) {
 
-                    }
-                } catch (IOException ex) {
-                    Logger.getLogger(LogedWindowController.class.getName())
-                            .log(Level.SEVERE, null, ex);
                 }
-
+            } catch (IOException ex) {
+                Logger.getLogger(LogedWindowController.class.getName())
+                        .log(Level.SEVERE, null, ex);
             }
 
         }
+
+     }
+    /**
+     * User name validation length. If field is too long than 15 characters, show error message.
+     * Also checks that it does not contain special characters
+     * @param observable The value being observed.
+     */
+    private void userNameLength(ObservableValue observable, String oldValue,
+             String newValue) {
+        String regex = "^[a-zA-Z1-9]*$";
+        try {
+            if (userNameTxTF.getText().length() > 15) {
+                new Alert(AlertType.ERROR, "La longitud del campo user supera los 15 caracteres").showAndWait();
+            } else if (!userNameTxTF.getText().matches(regex)) {
+                new Alert(AlertType.ERROR, "El campo contiene caracteres especiales").showAndWait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       
+
     }
+    /**
+     * Password validation length. If field is less than 6 or long than 12 characters, show error message.
+     * @param observable The value being observed.
+     */
+    private void passwordLength(ObservableValue observable,String oldValue,
+             String newValue) {
+        try {
+            if (passwdTxTF.getText().length() < 6 || passwdTxTF.getText().length() > 12) {
+                new Alert(AlertType.ERROR, "El campo password es minimo de 6 caracteres o maximo de 12").showAndWait();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
 
-
-
+    }
+   
+    }
