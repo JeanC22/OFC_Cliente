d9a7db83c02f7729886a8cd8b297a8230fc1d813
Jeanpierr <76107258+JeanC22@users.noreply.github.com>
2022-11-10 12:36:29 +0100

    TERMINADO

--- OFC_Cliente/src/model/controllers/SignInWindowController.java
+++ OFC_Cliente/src/model/controllers/SignInWindowController.java
@@ -169,21 +169,19 @@ public class SignInWindowController {
         try {
             //Validar que el campo userName (userNameTxTF) y el campo password (passwdTxTF) están informados:
             if (userNameTxTF.getText().isEmpty() || passwdTxPF.getText().isEmpty()) {
-                userNameTxTF.setFocusTraversable(true);
-
+                userNameTxTF.requestFocus();
                 throw new Exception("Todos los campos no estan informados");
             }
             //Validar la longitud userName (userNameTxTF) máximo 15 caracteres:
 
             if (userNameTxTF.getText().length() > 15) {
-                userNameTxTF.setFocusTraversable(true);
-
+                userNameTxTF.requestFocus();
                 throw new Exception("La longitud del "
                         + "campo user supera los 15 caracteres");
             }
             //Validar que el userName (userNameTxTF) no cuenta con caracteres especiales:
             if (!userNameTxTF.getText().matches(regex)) {
-                userNameTxTF.setFocusTraversable(true);
+                userNameTxTF.requestFocus();
                 throw new Exception(
                         "El campo contiene"
                         + " caracteres especiales");
@@ -192,7 +190,7 @@ public class SignInWindowController {
             //Validar que la longitud del password(passwdTxTF) es máximo 12 caracteres
 
             if (passwdTxPF.getText().length() < 6 || passwdTxPF.getText().length() > 12) {
-                passwdTxPF.setFocusTraversable(true);
+                passwdTxPF.requestFocus();
                 throw new Exception(
                         "El campo password es minimo "
                         + "de 6 caracteres o maximo de 12");
